dc87625d3a819c1a5ee10cd52ad5f75a878d2276
Artur Tsahikian <atsahikian@gmail.com>
2021-06-26 20:24:06 +0300

    Generate stub bin file at build time

--- pe-protector/main.cpp
+++ pe-protector/main.cpp
@@ -17,16 +17,23 @@ int main(int argc, char* argv[], char* env[]) {
   int exitCode = 0;
   if (argc == 2) {
     std::error_code ec{};
-    std::filesystem::copy(argv[1], (argv[1] + std::string(".bak")).c_str(), ec);
+    const std::filesystem::path sourceFile{argv[1]};
+    const std::filesystem::path targetFile{
+        std::filesystem::path{sourceFile}.replace_filename(
+            std::string("protected-") + sourceFile.filename().string())};
+
+    std::filesystem::copy(argv[1], targetFile.string(),
+                          std::filesystem::copy_options::overwrite_existing,
+                          ec);
     // TODO: check
     if (!ec) {
       try {
         const NPeProtector::SClientFile clientFile =
-            NPeProtector::getPeFileInfo(argv[1]);
+            NPeProtector::getPeFileInfo(targetFile.string().c_str());
         // test
         NPeProtector::gImageBase = clientFile.mImageBase;
 
-        std::ofstream fileStream(argv[1],
+        std::ofstream fileStream(targetFile.string(),
                                  std::ios_base::binary | std::ios_base::trunc);
 
         if (fileStream.is_open()) {
@@ -34,7 +41,7 @@ int main(int argc, char* argv[], char* env[]) {
 
           NPeProtector::protectPe(fileStream, clientFile);
         } else {
-          printf("Failed to open file %s", argv[1]);
+          printf("Failed to open file %s", targetFile.string().c_str());
           exitCode = 1;
         }
       } catch (const std::exception& e) {
